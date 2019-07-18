5bbb86cfc0388aded600f48cb442907b64d87976
joaquimbarreto <joaquim@barreto.co>
2019-07-18 15:50:55 +0100

    Initial commit2

--- client/src/translationsAPI.js
+++ client/src/translationsAPI.js
@@ -1,16 +1,16 @@
 class translationsAPI {
-	// static init() {
-	// 	process.env.REACT_APP_STAGE === "dev"
-	// 		? (this.base_URL = "http://localhost:3001")
-	// 		: (this.base_URL = "https://www.piglatin.app");
-	// }
+	static init() {
+		process.env.REACT_APP_STAGE === "dev"
+			? (this.base_URL = "http://localhost:3001")
+			: (this.base_URL = "https://www.piglatin.app");
+	}
 	static async translations() {
-		const res = await fetch("http://localhost:3001/translations");
+		const res = await fetch(this.base_URL + "/translations");
 		return await res.json();
 	}
 
 	static deleteTranslation(id) {
-		return fetch("http://localhost:3001/delete_translation", {
+		return fetch(this.base_URL + "/delete_translation", {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json"
@@ -20,7 +20,7 @@ class translationsAPI {
 	}
 
 	static async createTranslation(translation) {
-		const res = await fetch("http://localhost:3001/create_translation", {
+		const res = await fetch(this.base_URL + "/create_translation", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json"
@@ -31,6 +31,6 @@ class translationsAPI {
 	}
 }
 
-// translationsAPI.init();
+translationsAPI.init();
 
 export default translationsAPI;
